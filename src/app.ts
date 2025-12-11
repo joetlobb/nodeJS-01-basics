789af2aa8da7cfbed4478cb53302bb7e7643030f
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-12-10 21:40:37 -0800

    Understanding request

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import { createServer, IncomingMessage, ServerResponse } from "http"; // TS
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit()
 });
 
 server.listen(3000);
