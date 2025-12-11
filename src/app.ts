604ae0ea12378b97b8f6ceda1be7f4a6457f5805
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-12-10 21:41:33 -0800

    Sending responses

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,13 @@ import { createServer, IncomingMessage, ServerResponse } from "http"; // TS
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   console.log(req.url, req.method, req.headers);
   // process.exit()
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My first page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
