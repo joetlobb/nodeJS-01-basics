1b39a43af7ce6014f89034aea0547f5ce7d34692
Gulyapas Poonkawinsiri <67386394+joetlobb@users.noreply.github.com>
2025-12-10 21:43:42 -0800

    Routing requests

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,19 @@
 import { createServer, IncomingMessage, ServerResponse } from "http"; // TS
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  console.log(req.url, req.method, req.headers);
+  //   console.log(req.url, req.method, req.headers);
   // process.exit()
+
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
